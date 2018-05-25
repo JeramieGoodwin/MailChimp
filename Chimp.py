da2f9d8deec36c05c7af99df9ad400ff56447d6b
Jeramie Goodwin <jeramie.goodwin623@gmail.com>
2018-05-25 15:21:13 -0700

    cleaned up functions

--- Chimp.py
+++ Chimp.py
@@ -4,8 +4,9 @@ Created on Mon May 21 15:22:00 2018
 @author: jeramie.goodwin
 
 TODO:
-* add api key as a parameter in mailchimp
-* add adjustable offsets and limits to get_campaign & get_email_activity
+    * Speed up email acitivity module
+        > Use generator?
+        > batch process?
 """
 import sys
 from urllib import parse
@@ -38,25 +39,17 @@ class MailChimp:
         self.version = str(version)
         self.api_root = "https://" + self.shard + ".api.mailchimp.com/" + self.version +"/"
 
-    def get_json(self, params=None, dest=None):
+    def get_json(self,dest_key=None,params=None,endpoint=None):
         """
-            This function takes as input an api key, root url, paramerters,
-            and a destination.
-
-            Returned is a JSON string from the MailChimp endpoint url with the fields
-            prescribed in the API parameter dictionary (params)
-
-            INPUT:
+            ARGS:
                 apikey {str} = MailChimp user key
-                root {url-str} = API GET call root url
                 params {dict} = Query parameters and fields passed to the API call
-                dest = {url-str} = Page/form destination
+                endpoint {str}
+                key {str}
 
             OUTPUT:
                 JSON
             """
-        endpoint = parse.urljoin(self.api_root,dest)
-
         if params is not None:
 
             response = requests.get(endpoint, params=params,
@@ -74,10 +67,6 @@ class MailChimp:
             except ValueError:
                 print("Cannot decode json, got %s" )% response.text
 
-            if len(body[dest]) == 0:
-
-                print('Nothing found')
-
         else:
             response = requests.get(endpoint,
                                     auth=("apikey",self.apikey),
@@ -93,17 +82,14 @@ class MailChimp:
 
             except ValueError:
                 print("Cannot decode json, got %s" )% response.text
+        
+        data = body[dest_key]
 
-            if len(body[dest]) == 0:
+        return data
 
-                print('Nothing found')
-
-        return body
-
-    def get_campaign(self,dest='campaigns'):
+    def get_campaign(self,dest='campaigns',
+                     params={'fields':'campaigns.id,campaigns.settings.title,campaigns.send_time'}):
         """
-            This function returns a list of all campign ids associated with the
-            apikey.
             ARGS:
                 apikey {str} = MailChimp user key
                 params {dict} = Query parameters and fields passed to the API call
@@ -113,14 +99,10 @@ class MailChimp:
                 Pandas data frame -
                 campaign_id, send_time, campaign _title
             """
-            
-        params={'fields':'campaigns.id,\
-                          campaigns.settings.title,\
-                          campaigns.send_time'}
-        
-        r = self.get_json(params=params,dest=dest)
-        camps_dict = r[dest][0:]
+        ep = parse.urljoin(self.api_root,dest)
 
+        camps_dict = self.get_json(params=params,dest_key=dest,endpoint=ep)
+        
         camp_lst = []
 
         for i in camps_dict:
@@ -134,49 +116,30 @@ class MailChimp:
 
         return camp_info
 
-    def get_email_activity(self,camp_ids = [], count=500,dest='email_activity'):
+    def get_activity(self,camp_ids = [], count=500,dest='email_activity'):
         """
-        This function returns a nested dictionary defined by the input paramaters
-        from the URL endpoint.
-
-        INPUT:
-                apikey {str} = MailChimp user key
+        ARGS:
                 params {dict} = Query parameters and fields passed to the API call
-                camp_ids {tuple} = List of campaign ids returned
+                camp_ids {list} = List of campaign ids returned
                                     from the get_campaign() function OR a Series
 
         OUTPUT:
                 user_activity = Pandas dataframe of user activity organized by
                 user email and campaing id
-
         """
         params={'count':count,
-                                   'fields':'emails.campaign_id,\
-                                    emails.email_address,'\
-                                   'emails.activity,emails.activity.action'}
+                'fields':'emails.campaign_id,emails.email_address,''emails.activity,emails.activity.action'}
+        
         activity = []
         camp_info = self.get_campaign()
-        for i,c in enumerate(camp_ids):
-            endpoint = self.apiroot+c+'/'+dest
-            print(endpoint)
-            r  = requests.get(endpoint,params=params,
-                          auth=('apikey',self.apikey),
-                          verify=True)
-
-            try:
-                r.raise_for_status()
-                r = r.json()
-                print("Success!")
-
-            except requests.exceptions.HTTPError as err:
-                print("Error: {} {}").format(str(r.status_code), err)
-                print(json.dumps(r.json(), indent=4))
-
-            except ValueError:
-                print("Cannot decode json, got %s" )% r.text
-
-            emails = r['emails']
-
+        
+        for i,c in enumerate(camp_info['campaign_id']):
+            ep = 'https://us17.api.mailchimp.com/3.0/reports/'+c+'/email-activity'
+            print(ep)
+            emails = self.get_json(dest_key='emails',
+                         endpoint=ep,
+                         params=params)
+            
             for j,user in enumerate(emails):
                 if len(user['activity']) == 0:
                     df = pd.DataFrame(
@@ -185,29 +148,27 @@ class MailChimp:
                              'email_address':user['email_address'],
                              'ip':' ',
                              'timestamp':camp_info['send_time'][i]}, index=[0])
-
+                    
                 else:
                     df = pd.DataFrame.from_records(user)
                     df = pd.concat([df.drop('activity', axis=1),pd.DataFrame(df['activity'].tolist())], axis=1)
-
+            
                 activity.append(df)
+            
         user_activity = pd.concat(activity)
-
+        
         return user_activity
     
-    def get_subscriber(self,count=500, list_id='190ffb16f9'):
+    def get_members(self,count=500, list_id='190ffb16f9',dest='members'):
         """ Gets member list stats and other information"""
-        endpoint = parse.urljoin(self.apiroot,'lists/'+list_id+'/members')
-        
+        ep = parse.urljoin(self.api_root,'lists/'+list_id+'/'+dest)
+        print(ep)
         params = {'count':count,
                   'fields':'members.id,members.email_address,\
                    members.unique_email_id,members.status,members.stats,members.email_client,members.location'}
         
-        r  = requests.get(endpoint,params=params,
-                          auth=('apikey',self.apikey),
-                          verify=True)
-        r = r.json()
-        members = r['members']
+       
+        members = self.get_json(dest_key=dest,params=params,endpoint=ep)
         df = pd.DataFrame(members)
         member_data = pd.concat([df.drop(['location','stats'],axis=1),
                        pd.DataFrame(df['location'].tolist()),
